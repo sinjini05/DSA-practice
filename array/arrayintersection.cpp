baecae6fe4fa0870970f3ddf5c3138ded51fe15d
sinjini05 <98803426+sinjini05@users.noreply.github.com>
2023-02-06 23:52:43 +0530

    commit

--- array/arrayintersection.cpp
+++ array/arrayintersection.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool isSorted(int arr[],int N){
     for(int i=0;i<N;i++){
-        for(int j=i+1;j<N;j++){
+        for(int j=i+1;j<N;j++){    //arr[i]>arr[i+1]--- won't work bc we need to check further eles as well not just the one next to it
         if(arr[i]>arr[j]){
             return false;
         }}
