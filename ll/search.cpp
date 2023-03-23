3b459d85f02df49a975bd269da3017f59cef63be
sinjini05 <98803426+sinjini05@users.noreply.github.com>
2023-03-23 23:01:24 +0530

    commit

---
+++ ll/search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        Node* temp=head;
+        while(temp){
+            if(temp->data==key){
+                return true;
+            }
+            temp=temp->next;
+            
+        }
+        return false;
+       
+    }
+};
