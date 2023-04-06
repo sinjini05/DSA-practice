f241df0cc46dd7aae519f4564edb26cc1ef151a8
sinjini05 <98803426+sinjini05@users.noreply.github.com>
2023-04-07 02:07:51 +0530

    commit

---
+++ string/isRotated.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+
+    {
+
+        // Your code here
+
+        if(s1.size()!=s2.size())
+
+        return 0;
+
+        for(int i=0;i<s1.size();i++){
+
+            if(s1[i]==s2[0]){
+
+                string ans="";
+
+                ans=s1.substr(i,s1.size())+s1.substr(0,i);
+
+                if(ans==s2){
+
+                    return 1;
+
+                }
+
+            }
+
+        }
+
+        return 0;
+
+    }
+};
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s1,s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+    }
+    return 0;
+}
