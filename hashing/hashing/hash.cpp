870c5d10d004e1ffca8b0111624a5078169e029d
sinjini05 <98803426+sinjini05@users.noreply.github.com>
2023-04-12 22:51:41 +0530

    commit

---
+++ hashing/hashing/hash.cpp
@@ -0,0 +1,24 @@
+//pre-storing and fetching
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //precompute
+    int hash[13]={0};
+    
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+    }
+    return 0;
+}
